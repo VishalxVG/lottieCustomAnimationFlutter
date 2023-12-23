844f33a025e03b14a4474c22363168e3abc30734
Vishal Ghosh <avatarvishal1551@gmail.com>
2023-12-24 00:04:55 +0530

    Added animation

--- lib/homePage.dart
+++ lib/homePage.dart
@@ -7,9 +7,18 @@ class HomePage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      body: Center(
-        child: Lottie.network(
-            "https://lottie.host/89b6dc39-b466-48ae-9dc2-5416a3562262/E0L4ghmQ9l.json"),
+      body: Column(
+        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+        crossAxisAlignment: CrossAxisAlignment.center,
+        children: [
+          Lottie.asset("assets/ProfileAnimation.json", width: 100, height: 100),
+          const Text(
+            "Merry Christmas",
+            style: TextStyle(fontSize: 30),
+          ),
+          Lottie.network(
+              "https://lottie.host/89b6dc39-b466-48ae-9dc2-5416a3562262/E0L4ghmQ9l.json"),
+        ],
       ),
     );
   }
